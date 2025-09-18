7d6a63732dfd39c43bedf759f31485cb2778291f
RAJ SACHAN <raj.24bce10022@vitbhopal.ac.in>
2025-09-18 22:33:15 +0530

    Create Grade enum with point values

---
+++ src/edu/ccrm/domain/Grade.java
@@ -0,0 +1,11 @@
+package edu.ccrm.domain;
+
+public enum Grade {
+    S(10), A(9), B(8), C(7), D(6), E(5), F(0);
+
+    private final int points;
+    Grade(int p){ this.points = p; }
+    public int getPoints(){ return points; }
+    @Override
+    public String toString(){ return name(); }
+}
